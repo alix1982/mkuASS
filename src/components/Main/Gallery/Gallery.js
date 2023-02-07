acb18a084a423ea49ad4b0e29a108e5669317e92
Alix2019 <alix1982@yandex.ru>
2023-02-07 22:03:41 +0300

    removed_error_Eslint

--- src/components/Main/Gallery/Gallery.js
+++ src/components/Main/Gallery/Gallery.js
@@ -2,119 +2,124 @@ import React from 'react';
 import {useEffect, useState} from 'react';
 // import { Link } from 'react-router-dom';
 
-// import foto1 from '../../../images/gallery/1.jpg';
-// import foto2 from '../../../images/gallery/2.jpg';
-// import foto3 from '../../../images/gallery/3.jpg';
-// import foto4 from '../../../images/gallery/4.jpg';
-// import foto5 from '../../../images/gallery/5.jpg';
-// import foto6 from '../../../images/gallery/6.jpg';
-import buttonLeft from '../../../images/buttonLeft.svg';
-import buttonRight from '../../../images/buttonRight.svg';
+// import photo1 from '../../../images/gallery/1.jpg';
+// import photo2 from '../../../images/gallery/2.jpg';
+// import photo3 from '../../../images/gallery/3.jpg';
+// import photo4 from '../../../images/gallery/4.jpg';
+// import photo5 from '../../../images/gallery/5.jpg';
+// import photo6 from '../../../images/gallery/6.jpg';
+// import buttonLeft from '../../../images/buttonLeft.svg';
+// import buttonRight from '../../../images/buttonRight.svg';
 import gallery from '../../../utils/gallery';
 
 function Gallery (props) {
   const galleryArr = gallery()
-  let indexFoto=1;
+  let indexPhoto=1;
   const lengthGalleryArr = galleryArr.length;
-  const [currentFoto, setCurrentFoto] = useState(galleryArr[0]);
-  const [currentFotoNext, setCurrentFotoNext] = useState(galleryArr[1]);
-  const [currentFotoPrevious, setCurrentFotoPrevious] = useState(galleryArr[lengthGalleryArr - 1]);
+  const [currentPhoto, setCurrentPhoto] = useState(galleryArr[0]);
+  const [currentPhotoNext, setCurrentPhotoNext] = useState(galleryArr[1]);
+  const [currentPhotoPrevious, setCurrentPhotoPrevious] = useState(galleryArr[lengthGalleryArr - 1]);
   const [counterGallery, setCounterGallery] = useState(1);
-  const [isAnimationOn, setisAnimationOn] = useState(false);
+  // const [isAnimationOn, setisAnimationOn] = useState(false);
 
-  // console.log(currentFoto);
-  function chooseFoto(){
-    switch (currentFoto) {
+  
+  // console.log(currentPhoto);
+  function choosePhoto(){
+    switch (currentPhoto) {
       case galleryArr[0]:
-        setCurrentFoto(galleryArr[1]);
-        setCurrentFotoNext(galleryArr[2]);
-        setCurrentFotoPrevious(galleryArr[0]);
+        setCurrentPhoto(galleryArr[1]);
+        setCurrentPhotoNext(galleryArr[2]);
+        setCurrentPhotoPrevious(galleryArr[0]);
         setCounterGallery(2);
         break;
       case galleryArr[1]:
-        setCurrentFoto(galleryArr[2]);
-        setCurrentFotoNext(galleryArr[3]);
-        setCurrentFotoPrevious(galleryArr[1]);
+        setCurrentPhoto(galleryArr[2]);
+        setCurrentPhotoNext(galleryArr[3]);
+        setCurrentPhotoPrevious(galleryArr[1]);
         setCounterGallery(3);
         break;
       case galleryArr[2]:
-        setCurrentFoto(galleryArr[3]);
-        setCurrentFotoNext(galleryArr[0]);
-        setCurrentFotoPrevious(galleryArr[2]);
+        setCurrentPhoto(galleryArr[3]);
+        setCurrentPhotoNext(galleryArr[0]);
+        setCurrentPhotoPrevious(galleryArr[2]);
         setCounterGallery(4);
         break;
-      // case foto4:
-      //   setCurrentFoto(foto5);
+      // case photo4:
+      //   setCurrentPhoto(photo5);
       //   break;
       case galleryArr[3]:
-        setCurrentFoto(galleryArr[0]);
-        setCurrentFotoNext(galleryArr[1]);
-        setCurrentFotoPrevious(galleryArr[lengthGalleryArr - 1]);
+        setCurrentPhoto(galleryArr[0]);
+        setCurrentPhotoNext(galleryArr[1]);
+        setCurrentPhotoPrevious(galleryArr[lengthGalleryArr - 1]);
         setCounterGallery(1);
         break;
-      // case foto6:
-      //   setCurrentFoto(foto1);
+      // case photo6:
+      //   setCurrentPhoto(photo1);
       //   break;
       default:
-        setCurrentFoto(galleryArr[0]);
-        setCurrentFotoNext(galleryArr[1]);
-        setCurrentFotoPrevious(galleryArr[lengthGalleryArr - 1]);
+        setCurrentPhoto(galleryArr[0]);
+        setCurrentPhotoNext(galleryArr[1]);
+        setCurrentPhotoPrevious(galleryArr[lengthGalleryArr - 1]);
         setCounterGallery(1);
         break;
   }}
 
-  const switchingFoto = setTimeout(()=>{chooseFoto()}, 10000);
-  useEffect(()=>{switchingFoto},[counterGallery]);
+  function switchingPhoto () {setTimeout(choosePhoto, 5000);}
+  useEffect(switchingPhoto,[counterGallery]);
 
-  function setFotoGallery (endArray, valueEndArray) {
+  function setPhotoGallery (endArray, valueEndArray) {
     galleryArr.forEach((item, index)=>{
-      if (item === currentFoto) {indexFoto=index}
+      if (item === currentPhoto) {indexPhoto=index}
     });
-    if (indexFoto === endArray) { setCurrentFoto(galleryArr[valueEndArray]) } 
-    else { setCurrentFoto(galleryArr[(endArray === (galleryArr.length-1)) ? (indexFoto+1) : (indexFoto -1)]) }
-    clearTimeout(switchingFoto)
+    if (indexPhoto === endArray) { setCurrentPhoto(galleryArr[valueEndArray]) } 
+    else { setCurrentPhoto(galleryArr[(endArray === (galleryArr.length-1)) ? (indexPhoto+1) : (indexPhoto -1)]) }
+    clearTimeout(choosePhoto);
   }
 
-  function setFotoNext (endArray, valueEndArray) {
+  function setPhotoNext (endArray, valueEndArray) {
     galleryArr.forEach((item, index)=>{
-      if (item === currentFotoNext) {indexFoto=index}
+      if (item === currentPhotoNext) {indexPhoto=index}
     });
-    if (indexFoto === endArray) { setCurrentFotoNext(galleryArr[valueEndArray]) } 
-    else { setCurrentFotoNext(galleryArr[(endArray === (galleryArr.length-1)) ? (indexFoto+1) : (indexFoto -1)]) }
-    clearTimeout(switchingFoto)
+    if (indexPhoto === endArray) { setCurrentPhotoNext(galleryArr[valueEndArray]) } 
+    else { setCurrentPhotoNext(galleryArr[(endArray === (galleryArr.length-1)) ? (indexPhoto+1) : (indexPhoto -1)]) }
+    clearTimeout(switchingPhoto)
   }
   
-  function setFotoPrevious (endArray, valueEndArray) {
+  function setPhotoPrevious (endArray, valueEndArray) {
     galleryArr.forEach((item, index)=>{
-      if (item === currentFotoPrevious) {indexFoto=index}
+      if (item === currentPhotoPrevious) {indexPhoto=index}
     });
-    if (indexFoto === endArray) { setCurrentFotoPrevious(galleryArr[valueEndArray]) } 
-    else { setCurrentFotoPrevious(galleryArr[(endArray === (galleryArr.length-1)) ? (indexFoto+1) : (indexFoto -1)]) }
-    clearTimeout(switchingFoto)
+    if (indexPhoto === endArray) { setCurrentPhotoPrevious(galleryArr[valueEndArray]) } 
+    else { setCurrentPhotoPrevious(galleryArr[(endArray === (galleryArr.length-1)) ? (indexPhoto+1) : (indexPhoto -1)]) }
+    clearTimeout(switchingPhoto)
   }
 
-  function onSwitchingFotoRight() {
+  function onSwitchingPhotoRight() {
     // setisAnimationOn(true);
-    setFotoGallery (galleryArr.length-1, 0);
-    setFotoNext (galleryArr.length-1, 0);
-    setFotoPrevious (galleryArr.length-1, 0);
+    setPhotoGallery (galleryArr.length-1, 0);
+    setPhotoNext (galleryArr.length-1, 0);
+    setPhotoPrevious (galleryArr.length-1, 0);
     // setisAnimationOn(false);
     // setTimeout(()=>{setisAnimationOn(false)}, 5000)
   }
-  function onSwitchingFotoLeft() {
-    setFotoGallery (0, galleryArr.length-1);
-    setFotoNext (0, galleryArr.length-1);
-    setFotoPrevious (0, galleryArr.length-1);
+  function onSwitchingPhotoLeft() {
+    setPhotoGallery (0, galleryArr.length-1);
+    setPhotoNext (0, galleryArr.length-1);
+    setPhotoPrevious (0, galleryArr.length-1);
   }
 
   return (
       <section className="gallery">
-        <button className={`gallery__buttonLeft ${isAnimationOn ? "gallery__animation" : ""}`} onClick={onSwitchingFotoLeft}>
-          <img src={currentFotoPrevious} className="gallery__buttonLeftImg"/>
+        {/* <button className={`gallery__buttonLeft ${isAnimationOn ? "gallery__animation" : ""}`} onClick={onSwitchingPhotoLeft}> */}
+        <button className="gallery__buttonLeft" onClick={onSwitchingPhotoLeft}>
+
+          <img src={currentPhotoPrevious} className="gallery__buttonLeftImg" alt="предыдущее фото"/>
         </button>
-        <img src={currentFoto} className="gallery__foto" alt="фото"/>
-        <button className={`gallery__buttonRight ${isAnimationOn ? "gallery__animation" : ""}`} onClick={onSwitchingFotoRight}>
-          <img src={currentFotoNext} className="gallery__buttonRightImg"/>
+        <img src={currentPhoto} className="gallery__photo" alt="фото"/>
+        {/* <button className={`gallery__buttonRight ${isAnimationOn ? "gallery__animation" : ""}`} onClick={onSwitchingPhotoRight}> */}
+        <button className="gallery__buttonRight" onClick={onSwitchingPhotoRight}>
+
+          <img src={currentPhotoNext} className="gallery__buttonRightImg" alt="следующее фото"/>
         </button>
         <p className="gallery__text">Муниципальное казенное учреждение <br/>"Аварийно-спасательная служба Северодвинска</p>
       </section>
