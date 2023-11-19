527abe38684307092043495f2af135c9223f19dc
Alix2019 <alix1982@yandex.ru>
2023-11-19 10:30:27 +0300

    fix_plan komplektovania_and_podgotovki_2022->2024

--- src/utils/documentsListPlans.js
+++ src/utils/documentsListPlans.js
@@ -1,38 +1,38 @@
 // import React from 'react';
 import icon_doc_1 from '../images/icon_doc_1.png';
-import docPlanKomplektovaniya2023 from './documents/documentsPlans/Plan-komplektovaniya-na-2023-god_rotated.pdf';
-import docPlanKomplektovaniya2022 from './documents/documentsPlans/Plan-komplektovaniya-2022-god.pdf';
+import docPlanKomplektovaniya2024 from './documents/documentsPlans/Plan-komplektovaniya-2024-god.pdf';
+import docPlanKomplektovaniya2023 from './documents/documentsPlans/Plan-komplektovaniya-2023-god_rotated.pdf';
+import docPlanPodgotovki2024 from './documents/documentsPlans/plan-podgotovki-na-2024-god.docx';
 import docPlanPodgotovki2023 from './documents/documentsPlans/plan-podgotovki-na-2023-god.docx';
-import docPlanPodgotovki2022 from './documents/documentsPlans/plan-podgotovki-dolzhnostnyh-licz-na-2022-god.pdf';
 // import docSanepid from './documents/documentsMKU/Sanepid-zaklyuchenie-001.pdf';
 
 const documentsListPlans = [
   {
     titleDoc:
-      'План комплектования курсов гражданской обороны, защиты от чрезвычайных ситуаций и пожарной безопасности Сeвeродвинска на 2023 год',
+      'План комплектования курсов гражданской обороны, защиты от чрезвычайных ситуаций и пожарной безопасности на 2024 год',
     typeDoc: icon_doc_1,
-    path: docPlanKomplektovaniya2023,
+    path: docPlanKomplektovaniya2024,
     docId: 1,
   },
   {
     titleDoc:
-      'План комплектования курсов гражданской обороны, защиты от чрезвычайных ситуаций и пожарной безопасности на 2022 год',
+      'План комплектования курсов гражданской обороны, защиты от чрезвычайных ситуаций и пожарной безопасности на 2023 год',
     typeDoc: icon_doc_1,
-    path: docPlanKomplektovaniya2022,
+    path: docPlanKomplektovaniya2023,
     docId: 2,
+  },
+  {
+    titleDoc:
+      'План подготовки должностных лиц на курсах ГО Северодвинска на 2024 год',
+    typeDoc: icon_doc_1,
+    path: docPlanPodgotovki2024,
+    docId: 3,
   },
   {
     titleDoc:
       'План подготовки должностных лиц на курсах ГО Северодвинска на 2023 год',
     typeDoc: icon_doc_1,
     path: docPlanPodgotovki2023,
-    docId: 3,
-  },
-  {
-    titleDoc:
-      'План подготовки должностных лиц на Курсах ГО Северодвинска на 2022 год',
-    typeDoc: icon_doc_1,
-    path: docPlanPodgotovki2022,
     docId: 4,
   },
   // {
